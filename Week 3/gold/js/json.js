baaee96c4395df97e04deef961429a58758ab360
Craig Schultz <caschultz2005@gmail.com>
2013-06-16 20:31:14 -0700

    Start of Week 3

---
+++ Week 3/gold/js/json.js
@@ -0,0 +1,147 @@
+// Craig Schultz
+// Date:  June 9, 2013
+// Week 2
+// MiU 1306
+
+var json = {
+	"char1": {
+		"name": ["Name:", "Sondreman"],
+		"level": ["Level:", "90"],
+		"clas": ["Class:", "Druid"],
+		"race": ["Race:", "Night Elf"],
+		"ilevel": ["iLevel:", "511"]
+	},
+	"char2": {
+		"name": ["Name:", "Restosond"],
+		"level": ["Level:", "90"],
+		"clas": ["Class:", "Shaman"],
+		"race": ["Race:", "Dwarf"],
+		"ilevel": ["iLevel:", "510"]
+	},
+	"char3": {
+		"name": ["Name:", "Sondrepriest"],
+		"level": ["Level:", "90"],
+		"clas": ["Class:", "Priest"],
+		"race": ["Race:", "Worgen"],
+		"ilevel": ["iLevel:", "479"]
+	},
+	"char4": {
+		"name": ["Name:", "Sondfu"],
+		"level": ["Level:", "90"],
+		"clas": ["Class:", "Monk"],
+		"race": ["Race:", "Pandaren"],
+		"ilevel": ["iLevel:", "511"]
+	},
+	"char5": {
+		"name": ["Name:", "Sondsfury"],
+		"level": ["Level:", "90"],
+		"clas": ["Class:", "Warrior"],
+		"race": ["Race:", "Human"],
+		"ilevel": ["iLevel:", "449"]
+	},
+	"char6": {
+		"name": ["Name:", "Sondsmage"],
+		"level": ["Level:", "90"],
+		"clas": ["Class:", "Mage"],
+		"race": ["Race:", "Dwarf"],
+		"ilevel": ["iLevel:", "464"]
+	},
+	"char7": {
+		"name": ["Name:", "Kastun"],
+		"level": ["Level:", "90"],
+		"clas": ["Class:", "Paladin"],
+		"race": ["Race:", "Draenei"],
+		"ilevel": ["iLevel:", "438"]
+	},
+	"char8": {
+		"name": ["Name:", "Sondrezuhl"],
+		"level": ["Level:", "87"],
+		"clas": ["Class:", "Warlock"],
+		"race": ["Race:", "Gnome"],
+		"ilevel": ["iLevel:", "381"]
+	},
+	"char9": {
+		"name": ["Name:", "Sondrestabs"],
+		"level": ["Level:", "87"],
+		"clas": ["Class:", "Rogue"],
+		"race": ["Race:", "Worgen"],
+		"ilevel": ["iLevel:", "383"]
+	},
+	"char10": {
+		"name": ["Name:", "Sondre"],
+		"level": ["Level:", "86"],
+		"clas": ["Class:", "Death Knight"],
+		"race": ["Race:", "Human"],
+		"ilevel": ["iLevel:", "387"]
+	},
+	"char11": {
+		"name": ["Name:", "Elz"],
+		"level": ["Level:", "90"],
+		"clas": ["Class:", "Hunter"],
+		"race": ["Race:", "Night Elf"],
+		"ilevel": ["iLevel:", "520"]
+	},
+	"char12": {
+		"name": ["Name:", "Elzors"],
+		"level": ["Level:", "90"],
+		"clas": ["Class:", "Shaman"],
+		"race": ["Race:", "Draenei"],
+		"ilevel": ["iLevel:", "515"]
+	},
+	"char13": {
+		"name": ["Name:", "Elzfury"],
+		"level": ["Level:", "90"],
+		"clas": ["Class:", "Warrior"],
+		"race": ["Race:", "Worgen"],
+		"ilevel": ["iLevel:", "480"]
+	},
+	"char14": {
+		"name": ["Name:", "Elzzie"],
+		"level": ["Level:", "90"],
+		"clas": ["Class:", "Monk"],
+		"race": ["Race:", "Pandaren"],
+		"ilevel": ["iLevel:", "470"]
+	},
+	"char15": {
+		"name": ["Name:", "Elzicle"],
+		"level": ["Level:", "90"],
+		"clas": ["Class:", "Mage"],
+		"race": ["Race:", "Gnome"],
+		"ilevel": ["iLevel:", "460"]
+	},
+	"char16": {
+		"name": ["Name:", "Sneakyelz"],
+		"level": ["Level:", "80"],
+		"clas": ["Class:", "Rogue"],
+		"race": ["Race:", "Gnome"],
+		"ilevel": ["iLevel:", "350"]
+	},
+	"char17": {
+		"name": ["Name:", "Elzabet"],
+		"level": ["Level:", "90"],
+		"clas": ["Class:", "Druid"],
+		"race": ["Race:", "Worgen"],
+		"ilevel": ["iLevel:", "450"]
+	},
+	"char18": {
+		"name": ["Name:", "Elzffliction"],
+		"level": ["Level:", "85"],
+		"clas": ["Class:", "Warlock"],
+		"race": ["Race:", "Dwarf"],
+		"ilevel": ["iLevel:", "300"]
+	},
+	"char19": {
+		"name": ["Name:", "Elzshadow"],
+		"level": ["Level:", "85"],
+		"clas": ["Class:", "Priest"],
+		"race": ["Race:", "Worgen"],
+		"ilevel": ["iLevel:", "366"]
+	},
+	"char20": {
+		"name": ["Name:", "Sondresting"],
+		"level": ["Level:", "90"],
+		"clas": ["Class:", "Hunter"],
+		"race": ["Race:", "Night Elf"],
+		"ilevel": ["iLevel:", "519"]
+	}
+}
